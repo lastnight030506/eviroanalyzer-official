88c7c8502b53f8f5bcd9cb276fe673ad20dd0b39
lastnight030506 <minh03052006@gmail.com>
2026-04-24 17:43:47 +0700

    Remove all TS/Tauri visual files and old R Shiny duplicate

--- components/dialogs/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as DescriptivesDialog } from './DescriptivesDialog';
-export { default as FrequenciesDialog } from './FrequenciesDialog';
-export { default as TTestDialog } from './TTestDialog';
-export { default as ANOVADialog } from './ANOVADialog';
-export { default as CorrelationDialog } from './CorrelationDialog';
-export { default as RegressionDialog } from './RegressionDialog';
-export { default as PlotDialog } from './PlotDialog';
